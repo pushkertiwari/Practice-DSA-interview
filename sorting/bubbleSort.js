4c7c478fd3df9e21975caf1ec8e573a688486a62
pushkertiwari <tiwaripushker@gmail.com>
2026-06-20 14:05:49 +0530

    added bubble sort

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,27 @@
+// Time Complexity: O(n) (Best Case) when array is already sorted (optimized with isSwapped).
+
+// Worst Case: O(n2) When array is in reverse order.
+
+// Space Complexity: O(1) In-place sorting, no extra space used.
+let arr = [4,5,1,3,9];
+
+function bubbleSort(arr) {
+    let n = arr.length;
+    for(let i = 0; i < n-1; i++) {
+      let isSwapped = false;
+      for(let j = 0; j < n-1-i; j++) {
+        if(arr[j]>arr[j+1]) {
+          let temp=arr[j]
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+          isSwapped=true;
+        }
+      }
+      if(!isSwapped)
+        break;
+    }
+    return arr;
+}
+
+console.log(bubbleSort(arr));
+
